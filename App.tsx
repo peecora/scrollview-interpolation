fa3935abe666baf30a817c3325b0dc6feb3e1acf
peecora <pedro.pecora@gmail.com>
2022-11-15 16:38:56 +0000

    Final version

--- App.tsx
+++ App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+import { Page } from "./components/Page";
+
+const WORDS = ["Salve", "Salve", "Familia"];
 
 export default function App() {
+  const translateX = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    translateX.value = event.contentOffset.x;
+  });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Animated.ScrollView
+      style={styles.container}
+      horizontal
+      onScroll={scrollHandler}
+      scrollEventThrottle={16}
+    >
+      {WORDS.map((title, index) => {
+        return (
+          <Page
+            key={index.toString()}
+            index={index}
+            title={title}
+            translateX={translateX}
+          />
+        );
+      })}
+    </Animated.ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
